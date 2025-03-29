14a24b35ce7af614e692a26cf99ba181677fd958
Lester Cerioli <lesterlucasit@hotmail.com>
2025-03-29 06:44:08 -0700

    Initial repository

---
+++ src/test/java/com/medicalapp/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicalapp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
